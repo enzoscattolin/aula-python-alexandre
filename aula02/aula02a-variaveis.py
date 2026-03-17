d627d93bab0f8270f3dd1da2ecb604d829ced132
enzoscattolin <enzoscatolini@gmail.com>
2026-03-17 09:43:31 -0300

    sla

---
+++ aula02/aula02a-variaveis.py
@@ -0,0 +1,21 @@
+print("ola mundo")
+
+print(7+4)
+print("7 + 4")
+print("7" + "4")
+
+#variaveis
+
+nome = "Enzo" #str
+idade = 18 #int
+peso = 75.3 # float
+
+print(nome, idade, peso)
+
+#input - simula forms no cmd
+nome = input("digite seu nome:")
+idade = input("digite sua idade:")
+peso = input("digite seu peso:")
+
+print(nome, idade, peso)
+print(f"oiii {nome}!!!")
---
+++ aula02/desafios.py
@@ -0,0 +1,20 @@
+#desafio 1
+
+nome = input("Digite seu nome:")
+print(f"Boas vindas {nome}.")
+
+#desafio 2
+
+dia = input("Digite o dia do seu aniversario:")
+mes = input("Digite o mes do seu aniversario:")
+ano = input("Digite o ano do seu aniversario:")
+
+print(f"voce nasceu em: {dia}/{mes}/{ano})
+
+#desafio 3
+
+numeroum = input ("Digite o primeiro numero:")
+
+numerodois = input ("Digite o segundo numero:")
+
+print
